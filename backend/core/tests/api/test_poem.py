b833533346da23b611474cd58757ac658b10e505
Netervati <netervati@gmail.com>
2022-07-17 19:12:52 +0800

    Improve service test

--- backend/core/tests/api/test_poem.py
+++ backend/core/tests/api/test_poem.py
@@ -1,8 +1,13 @@
-from multiprocessing.sharedctypes import Array
-
-from django.http import JsonResponse
-from tests.fixtures import poem, user
 from django.urls import reverse
+
+
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
+)
+
+
+from tests.fixtures import poem, user
 
 
 import pytest
@@ -11,11 +16,12 @@ import pytest
 retrieve_poems_url = reverse("retrieve-poems")
 
 
+
 @pytest.mark.django_db
 def test_retrieve_poems_with_missing_params(client):
     response = client.get(retrieve_poems_url)
 
-    assert response.status_code == 400
+    assert response.status_code == HTTP_400_BAD_REQUEST
     assert response.data["errors"] == ["You are missing valid query parameters."]
 
 
@@ -23,15 +29,23 @@ def test_retrieve_poems_with_missing_params(client):
 def test_retrieve_poems_with_blank_params(client):
     response = client.get(retrieve_poems_url, data={"title": ""})
 
-    assert response.status_code == 400
+    assert response.status_code == HTTP_400_BAD_REQUEST
     assert response.data["errors"][0]["title"] == "Parameter should not be blank."
+
+
+@pytest.mark.django_db
+def test_retrieve_poems_with_no_return(client):
+    response = client.get(retrieve_poems_url, data={"title": "test"})
+
+    assert response.status_code == HTTP_200_OK
+    assert not response.data
 
 
 @pytest.mark.django_db
 def test_retrieve_poems(client, poem):
     response = client.get(retrieve_poems_url, data={"title": poem.title})
 
-    assert response.status_code == 200
+    assert response.status_code == HTTP_200_OK
     assert isinstance(response.data, list)
     assert response.data[0]["age"] == poem.age
     assert response.data[0]["author"] == poem.author
