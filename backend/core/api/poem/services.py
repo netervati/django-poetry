9ea4940155fee56b19213a78cb561e69bdbcf013
Netervati <netervati@gmail.com>
2022-07-18 00:50:54 +0800

    Add retrieve poems like endpoint

--- backend/core/api/poem/services.py
+++ backend/core/api/poem/services.py
@@ -1,6 +1,3 @@
-"""
-Retrieves the poems based exactly on the query parameters.
-"""
 from rest_framework.exceptions import ValidationError
 
 
@@ -9,6 +6,10 @@ from lib.utils import clean_params
 
 
 class RetrievePoemsExactService:
+    """
+    Retrieves the poems based exactly on the query parameters.
+    """
+
     def __init__(self, params):
         self.params = clean_params(params.query_params, Poem)
 
@@ -31,3 +32,40 @@ class RetrievePoemsExactService:
                 errors.append({f"{key}": f"Parameter should not be blank."})
 
         return errors
+
+
+class RetrievePoemsLikeService:
+    """
+    Retrieves the poems with attributes like query parameters.
+    """
+
+    def __init__(self, params):
+        self.params = clean_params(params.query_params, Poem)
+
+    def run(self):
+        errors = self.__validate_params()
+
+        if errors:
+            raise ValidationError(detail={"errors": errors})
+
+        return Poem.objects.filter(**self.__like_params())
+
+    # TO-DO feat_v1.0002: Create validation layer
+    def __validate_params(self):
+        errors = []
+        if len(self.params) == 0:
+            errors.append("You are missing valid query parameters.")
+
+        for key, val in self.params.items():
+            if not val.strip():
+                errors.append({f"{key}": f"Parameter should not be blank."})
+
+        return errors
+
+    def __like_params(self):
+        params = {}
+
+        for key, val in self.params.items():
+            params[f"{key}__icontains"] = val
+
+        return params
--- backend/core/tests/core/api/poem/test_poem_services.py
+++ backend/core/tests/core/api/poem/test_poem_services.py
@@ -11,36 +11,76 @@ from tests.fixtures import poem, user
 import pytest
 
 
-retrieve_poems_url = reverse("retrieve-poems")
+retrieve_poems_exact_url = reverse("retrieve-poems-exact")
+retrieve_poems_like_url = reverse("retrieve-poems-like")
 
 
 @pytest.mark.django_db
-def test_retrieve_poems_with_missing_params(client):
-    response = client.get(retrieve_poems_url)
+def test_retrieve_poems_exact_with_missing_params(client):
+    response = client.get(retrieve_poems_exact_url)
 
     assert response.status_code == HTTP_400_BAD_REQUEST
     assert response.data["errors"] == ["You are missing valid query parameters."]
 
 
 @pytest.mark.django_db
-def test_retrieve_poems_with_blank_params(client):
-    response = client.get(retrieve_poems_url, data={"title": ""})
+def test_retrieve_poems_exact_with_blank_params(client):
+    response = client.get(retrieve_poems_exact_url, data={"title": ""})
 
     assert response.status_code == HTTP_400_BAD_REQUEST
     assert response.data["errors"][0]["title"] == "Parameter should not be blank."
 
 
 @pytest.mark.django_db
-def test_retrieve_poems_with_no_return(client):
-    response = client.get(retrieve_poems_url, data={"title": "test"})
+def test_retrieve_poems_exact_with_no_return(client):
+    response = client.get(retrieve_poems_exact_url, data={"title": "test"})
 
     assert response.status_code == HTTP_200_OK
     assert not response.data
 
 
 @pytest.mark.django_db
-def test_retrieve_poems(client, poem):
-    response = client.get(retrieve_poems_url, data={"title": poem.title})
+def test_retrieve_poems_exact(client, poem):
+    response = client.get(retrieve_poems_exact_url, data={"title": poem.title})
+
+    assert response.status_code == HTTP_200_OK
+    assert isinstance(response.data, list)
+    assert response.data[0]["age"] == poem.age
+    assert response.data[0]["author"] == poem.author
+    assert response.data[0]["content"] == poem.content
+    assert response.data[0]["title"] == poem.title
+    assert response.data[0]["type"] == poem.type
+
+
+@pytest.mark.django_db
+def test_retrieve_poems_like_with_missing_params(client):
+    response = client.get(retrieve_poems_exact_url)
+
+    assert response.status_code == HTTP_400_BAD_REQUEST
+    assert response.data["errors"] == ["You are missing valid query parameters."]
+
+
+@pytest.mark.django_db
+def test_retrieve_poems_like_with_blank_params(client):
+    response = client.get(retrieve_poems_like_url, data={"title": ""})
+
+    assert response.status_code == HTTP_400_BAD_REQUEST
+    assert response.data["errors"][0]["title"] == "Parameter should not be blank."
+
+
+@pytest.mark.django_db
+def test_retrieve_poems_like_with_no_return(client):
+    response = client.get(retrieve_poems_like_url, data={"title": "test"})
+
+    assert response.status_code == HTTP_200_OK
+    assert not response.data
+
+
+@pytest.mark.django_db
+def test_retrieve_poems_like(client, poem):
+    substr_title = poem.title[0:1]
+
+    response = client.get(retrieve_poems_like_url, data={"title": substr_title})
 
     assert response.status_code == HTTP_200_OK
     assert isinstance(response.data, list)
