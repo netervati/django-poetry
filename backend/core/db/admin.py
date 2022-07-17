d2059f0575d7620d95bfc7ce43efcfae435de908
Netervati <netervati@gmail.com>
2022-07-17 21:59:36 +0800

    Add Age, Author, and Type models

--- backend/core/db/admin.py
+++ backend/core/db/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 
-from db.models import Poem
+from db.models import Age, Author, Poem, Type
 
 
-@admin.register(Poem)
+@admin.register(Age, Author, Poem, Type)
 class BaseAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
         return [field.name for field in self.model._meta.concrete_fields]
