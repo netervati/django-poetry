e04db7f0b2a0f63842c87ba0a4a763a2447cfb95
Netervati <netervati@gmail.com>
2022-07-20 02:08:28 +0800

    Add standing data handler in utils

--- backend/core/lib/utils.py
+++ backend/core/lib/utils.py
@@ -1,6 +1,7 @@
 """
 Serves as the module for utilities in the API.
 """
+import yaml
 
 
 def clean_params(kwargs, mapper):
@@ -16,3 +17,19 @@ def clean_params(kwargs, mapper):
             params[key] = val
 
     return params
+
+
+class StandingData:
+    def retrieve_ages(self):
+        return self.__load_yaml("ages")
+
+    def retrieve_types(self):
+        return self.__load_yaml("types")
+
+    def __load_yaml(self, key):
+        f = open(f"lib/yamls/standing_data.yml")
+        data = yaml.load(f, Loader=yaml.CLoader)
+
+        f.close()
+
+        return data[key]
