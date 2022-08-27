ebe9009d56597b89e8190685cd4ddc86c1b5a2c8
Netervati <netervati@gmail.com>
2022-08-28 03:14:04 +0800

    Use base path for age controller test

--- backend/core/tests/core/api/age/test_age_controller.py
+++ backend/core/tests/core/api/age/test_age_controller.py
@@ -8,6 +8,6 @@ base_path = "api.age.age_controller."
 
 def test_age_controller_retrieve(mocker, age):
     mocker.patch(f"{base_path}RetrieveAgesService.run").return_value = age
-    mocker.patch("api.age.age_controller.render").return_value = age
+    mocker.patch(f"{base_path}render").return_value = age
 
     assert_responses_match(AgeController().retrieve(""), age)
